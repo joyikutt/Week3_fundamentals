b1faf4fecf2e33239f8d7f1a39b18f2a01c109c5
Prabha Joyikutty <105876206+joyikutt@users.noreply.github.com>
2022-05-31 10:56:41 -0400

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -17,7 +17,7 @@ public class Week3 {
     public static void main(String[] args) {
         System.out.println("I am learning github");
        System.out.println("modified in the github"); 
-        //this is about fetch and merge
+        //this is about fetch and merge of the project
         
     }
     //week3 changes
